ace2f16d585ec13b190272791ee06a787d86fb9c
paragasu <jeffry.lunggot@trimajasa.com>
2016-08-10 16:57:31 +0800

    add return info

--- README.md
+++ README.md
@@ -18,11 +18,12 @@ local aws_auth_config = {
 }
 
 local email = ses:new(aws_auth_config)
-local sent  = email:send('hello@world.com', 'hello there', 'Sent using AWS Simple Email Service API') 
+local sent, info = email:send('hello@world.com', 'hello there', 'Sent using AWS Simple Email Service API') 
 
 if not sent then
-  ngx.say('Failed to send email')
-end
+  ngx.say('Failed: ' .. info)
+else
+  ngx.say('Sent')
 ```
 
 # Todo
--- lib/resty/email.lua
+++ lib/resty/email.lua
@@ -1,5 +1,11 @@
-local aws_auth = require 'aws_auth'
-local http = require 'requests'
+-- Lua resty module to send email via AWS SES API
+-- Author: Jeffry L. <paragasu@gmail.com>
+-- Website: github.com/paragasu/lua-resty-aws-email
+-- Licence: MIT
+
+local aws_auth = require 'resty.aws_auth'
+local request  = require 'requests'
+local http     = require 'resty.http'
 local email_from
 local config = {
   aws_service  = 'ses',  
@@ -21,10 +27,11 @@ function _M.new(self, c)
   config.aws_secret  = c.aws_secret
   config.aws_region  = c.aws_region
   config.aws_key     = c.aws_key
-  config.aws_host    = 'https://email.' .. c.aws_region .. '.amazonaws.com'
+  config.aws_host    = 'email.' .. c.aws_region .. '.amazonaws.com'
   return setmetatable(_M, mt)
 end
 
+local inspect = require 'inspect'
 
 -- send email
 function _M.send(self, email_to, subject, message)
@@ -32,23 +39,29 @@ function _M.send(self, email_to, subject, message)
     ['Action'] = 'SendEmail',
     ['Source'] = email_from,
     ['Destination.ToAddresses.member.1'] = email_to,
-    ['Message.Subject.Data']   = subject
+    ['Message.Subject.Data']   = subject,
     ['Message.Body.Text.Data'] = message
   }
 
   local aws = aws_auth:new(config)
-  local response = http.post({
-    url = aws_host,
-    header = {
+  local res = request.post('https://' .. config.aws_host, {
+    data = ngx.encode_args(config.request_body),
+    headers = {
       ['Content-Type'] = 'application/x-www-form-urlencoded', -- required
       ['Authorization'] = aws:get_authorization_header(),
       ['X-Amz-Date'] = aws:get_date_header()
     }
   })
 
-  local inspect = require 'inspect'
-  print(inspect(response))
+  local body, err = res.xml() 
+  local result = body.xml
+
+  if body.xml == 'SendEmailResponse' then
+    return true, body[1][1][1]  -- send
+  else
+    return false, body[1][3][1]  -- failed
+  end
 
 end
 
-ngx.say('SES done')
+return _M
