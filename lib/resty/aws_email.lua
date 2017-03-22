cbdf595ab7052ff98eb9651b61ab6a536b933ba5
paragasu <paragasu@gmail.com>
2017-03-22 10:18:39 +0800

    add email validation

--- lib/resty/aws_email.lua
+++ lib/resty/aws_email.lua
@@ -21,8 +21,11 @@ local mt  = { __index = _M }
 -- init with table contain required information
 -- to generate authorization header
 -- aws_key, aws_secret and aws_region
-function _M.new(self, c)
-  if not c then return ngx.log(ngx.ERR, 'Missing aws ses config') end
+function _M:new(c)
+  if not c then return error('Missing aws ses config') end
+  if not c.aws_secret then return error('Missing required aws_secret') end
+  if not c.aws_region then return error('Missing required aws_region') end
+  if not c.aws_key then return error('Missing required aws_key') end
   email_from = c.email_from
   config.aws_secret  = c.aws_secret
   config.aws_region  = c.aws_region
@@ -34,7 +37,10 @@ end
 local inspect = require 'inspect'
 
 -- send email
-function _M.send(self, email_to, subject, message)
+function _M:send(email_to, subject, message)
+  if not email_to or _M.check_valid_email(email_to) then error('Invalid email recipient') end
+  if not subject then error('Missing required email subject') end
+  if not message then error('Missing required email message') end
   config.request_body = {
     ['Action'] = 'SendEmail',
     ['Source'] = email_from,
@@ -62,6 +68,12 @@ function _M.send(self, email_to, subject, message)
     return false, body[1][3][1]  -- failed
   end
 
+end
+
+-- check for valid email
+function _M.check_valid_email(email)
+  local valid = string.match(email, '%w+@%w+[\.%w]+')
+  return valid == nil and false or true   
 end
 
 return _M
